d670218b231eadb61434d5868f90dbc5118f19b0
suprabha611@gmail.com <suprabha611@gmail.com>
2022-09-28 11:08:04 +0530

    Task 13

--- paramarr.cs
+++ paramarr.cs
@@ -11,18 +11,18 @@ namespace Csharp
     {
         public static int Multiplication(params int[] numbers)
         {
-            for (int i = 1; i < numbers.Length; i++)
+            for (int i = 1; i <= numbers.Length; i++)
             {
-                for (int j = 1; j < numbers.Length; j++)
+                for (int j = 1; j <= numbers.Length; j++)
                 {
-                    Console.WriteLine((i * j).ToString());
+                    Console.WriteLine("{0} X {1} ={2} \n", i, j, i * j);
                 }
             }
-            return(0);
+            return (0);  
         }
         public static void Main()
         {
-            int m = Multiplication( 1, 2, 3, 4, 5 );
+            int m = Multiplication( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         }
     }
 }
