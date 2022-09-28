a3fbb42c6f57813069aaec3ae29f1968724ad86e
suprabha611@gmail.com <suprabha611@gmail.com>
2022-09-28 16:06:21 +0530

    Task 13

---
+++ Outkey.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Csharp
+{
+    internal class Outkey
+    {
+        public static void Main()
+        {
+            int id;
+            string name, dept;
+            details(out id, out name, out dept);
+            Console.WriteLine($"Student ID= {id}");
+            Console.WriteLine($"Student Name= {name}");
+            Console.WriteLine($"Student Department= {dept}");
+
+        }
+        public static void details(out int sid, out string sname, out string sdept)
+        {
+            sid = 13072;
+            sname = "Suprabha";
+            sdept = ".NET";
+        }
+
+    }
+}
