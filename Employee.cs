4b08f113bb2c2fc0916296195d5bcb4b370b6bc7
suprabha611@gmail.com <suprabha611@gmail.com>
2022-09-28 15:12:12 +0530

    Task 12

--- Employee.cs
+++ Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,29 +11,32 @@ namespace Task_12
     {
         static void Main(string[] args)
         {
+            emp_det();
+        }
+        public static void emp_det()
+        {
+            int empid;
+            int n;
+            string empname;
             Console.WriteLine("Enter the number of employess:");
-            string val = Console.ReadLine();
-            int n = Convert.ToInt32(val);
-            //to store all employees id
-            String[] empid = new String[n];
-            //to store all employees name
-            String[] empname = new String[n];
-            //To read employee details
+            n = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Enter Employee " + (i + 1) + " ID");
-                empid[i] = Console.ReadLine();
-                Console.WriteLine("Enter Employee " +(i+1) + "Name");
-                empname[i] = Console.ReadLine();
+                Console.WriteLine("Enter Employee ID: ");
+                empid = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Employee Name: ");
+                empname = Console.ReadLine();
+                Employee e = new Employee();
+                e.display(empid, empname);
+                Console.WriteLine();
             }
-            //To dispaly the employee deatils
-            Console.WriteLine("Employee Details are");
-            //Console.WriteLine("-----------------------");
-            Console.WriteLine("ID\t Name");
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine(empid[i] + "\t" + empname[i]);
-            }
+        }
+        public void display(int empid, string empname)
+        {
+            Console.WriteLine("Employee Details are: ");
+            Console.WriteLine("Employee Id: {empid}");
+            Console.WriteLine("Employee name: {empname}");
+            
         }
     }
 }
