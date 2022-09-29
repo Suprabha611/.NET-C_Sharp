1f2a7d32bf3aef96b9c28c327773cd1679ae91af
suprabha611@gmail.com <suprabha611@gmail.com>
2022-09-29 10:44:36 +0530

    commit

---
+++ Task 14/inheritance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Csharp.Task_14
+{
+        public class Bonus
+        {
+            protected int bonus;
+        }
+        public class Staff : Bonus
+        {
+            protected int salary = 25000;
+            public void Staff_bonus()
+            {
+                bonus = 3 * salary;
+                Console.WriteLine($"Staff salary: {salary} and Bonus= { bonus}");
+            }
+        }
+        public class NonStaff : Staff
+        {
+            protected int salary = 15000;
+            public void NonStaff_bonus()
+            {
+                bonus = 2 * salary;
+                Console.WriteLine($"Non Staff Salary: {salary} and Bonus= {bonus}");
+            }
+        }
+    internal class inheritance
+    {
+        class Program
+        {
+            public static void Main()
+            {
+                Staff s = new Staff();
+                s.Staff_bonus();
+                NonStaff n = new NonStaff();
+                n.NonStaff_bonus();
+            }
+        }
+    }
+}
--- Task 14/mobile.cs
+++ Task 14/mobile.cs
@@ -16,14 +16,21 @@ namespace Csharp
         public static void number()
         {
             Console.WriteLine("Mobile Number validation");
-            string number1 = "990-286-1906";
-            var validate_number1 = Regex.IsMatch(number1, @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$");
-            Console.WriteLine($"Number is: {validate_number1}");
+            //string number1 = "990-286-1906";
+            //var validate_number1 = Regex.IsMatch(number1, @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$");
+            //Console.WriteLine($"Number is: {validate_number1}");
+            // string number2 = "690-286-1906";
+            //var validate_number2 = Regex.IsMatch(number2, @"^\(?([7-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$");
+            //Console.WriteLine($"Number is: {validate_number2}");
+
+                                    /*or*/
 
 
-            string number2 = "690-286-1906";
-            var validate_number2 = Regex.IsMatch(number2, @"^\(?([7-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$");
-            Console.WriteLine($"Number is: {validate_number2}");
+            Console.WriteLine("Enter the 10 digit mobile number");
+            //string mobile = "(9/8/7)389674670"--> Ture otherwise false
+            var number = (Console.ReadLine());
+            var validate_number = Regex.IsMatch(number, @"^[789]\d{9}$");
+            Console.WriteLine(validate_number);
         }
     }
 }
